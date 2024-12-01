6ac612f500ab6119e34ad85240197cf3043d36ed
Marcelo Costa <marcelodscosta@yahoo.com.br>
2024-11-30 23:18:49 -0300

    installing bcrypt for password encryption

--- src/main/java/com/marcelodscosta/todolist/user/UserController.java
+++ src/main/java/com/marcelodscosta/todolist/user/UserController.java
@@ -3,10 +3,7 @@ package com.marcelodscosta.todolist.user;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 @RestController
 @RequestMapping("/users")
@@ -27,4 +24,5 @@ public class UserController {
         var userCreated = this.userRepository.save(userModel);
         return ResponseEntity.status(HttpStatus.CREATED).body(userCreated);
     }
+
 }
