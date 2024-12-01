08b21757e4f564b9a1ba9fea4947380208c508c5
Marcelo Costa <marcelodscosta@yahoo.com.br>
2024-11-30 22:16:13 -0300

    implementing database access using repository

--- src/main/java/com/marcelodscosta/todolist/user/UserController.java
+++ src/main/java/com/marcelodscosta/todolist/user/UserController.java
@@ -1,5 +1,6 @@
 package com.marcelodscosta.todolist.user;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.PostMapping;
@@ -11,8 +12,12 @@ import org.springframework.web.bind.annotation.RestController;
 @RequestMapping("/users")
 public class UserController {
 
+    @Autowired
+    private IUserRepository userRepository;
+
     @PostMapping("/")
     public ResponseEntity create(@RequestBody UserModel userModel) {
-        return ResponseEntity.status(HttpStatus.CREATED).body(userModel);
+        var userCreated = this.userRepository.save(userModel);
+        return ResponseEntity.status(HttpStatus.CREATED).body(userCreated);
     }
 }
