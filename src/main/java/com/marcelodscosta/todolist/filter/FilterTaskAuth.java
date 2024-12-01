4cea5383d3e0a3f9404f66a63a65bae70c90f40b
Marcelo Costa <marcelodscosta@yahoo.com.br>
2024-12-01 19:06:55 -0300

    decoding the password

--- src/main/java/com/marcelodscosta/todolist/filter/FilterTaskAuth.java
+++ src/main/java/com/marcelodscosta/todolist/filter/FilterTaskAuth.java
@@ -1,9 +1,12 @@
 package com.marcelodscosta.todolist.filter;
 
+import at.favre.lib.crypto.bcrypt.BCrypt;
+import com.marcelodscosta.todolist.user.IUserRepository;
 import jakarta.servlet.FilterChain;
 import jakarta.servlet.ServletException;
 import jakarta.servlet.http.HttpServletRequest;
 import jakarta.servlet.http.HttpServletResponse;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Component;
 import org.springframework.web.filter.OncePerRequestFilter;
 
@@ -12,6 +15,10 @@ import java.util.Base64;
 
 @Component
 public class FilterTaskAuth extends OncePerRequestFilter {
+
+    @Autowired
+    IUserRepository userRepository;
+
     @Override
     protected void doFilterInternal(HttpServletRequest request, HttpServletResponse response, FilterChain filterChain) throws ServletException, IOException {
 
@@ -24,14 +31,26 @@ public class FilterTaskAuth extends OncePerRequestFilter {
         String auth = new String(decodedAuth); // Convertendo em String
         String[] authSplit = auth.split(":"); // Separando informação
 
-
-        System.out.printf("Usuário: %s%n", authSplit[0]);
-        System.out.printf("Senha: %s%n", authSplit[1]);
+        String username = authSplit[0];
+        String password = authSplit[1];
 
         // Validar Usuário
 
-        // Validar Senha
+        var user = this.userRepository.findByUsername(username);
+        if (user == null) {
+            response.sendError(HttpServletResponse.SC_UNAUTHORIZED);
+        } else {
+            // Validar Senha
 
-        // Seguir fluxo
+            var passwordVerify = BCrypt.verifyer().verify(password.toCharArray(), user.getPassword());
+            if(passwordVerify.verified){
+                filterChain.doFilter(request, response);
+            } else {
+                response.sendError(HttpServletResponse.SC_UNAUTHORIZED);
+            }
+            // Seguir fluxo
+        }
+
+
     }
 }
